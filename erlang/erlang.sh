a1c63553163992ea6cb6f9442a4f79edad0c624d
rkononov <rkononov@gmail.com>
2014-02-10 18:16:56 +0600

    using images

---
+++ erlang/erlang.sh
@@ -0,0 +1,1 @@
+erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell
---
+++ launch.sh
@@ -0,0 +1,23 @@
+iron_worker upload erlang/erlang
+iron_worker upload go/golang
+iron_worker upload java/java
+iron_worker upload mono/mono
+iron_worker upload node/node
+iron_worker upload php/php
+iron_worker upload python2/python2
+iron_worker upload python3/python3
+iron_worker upload ruby1.9/ruby1.9
+iron_worker upload ruby2.0/ruby2.0
+iron_worker upload scala/scala
+
+iron_worker queue erlang --wait
+iron_worker queue golang --wait
+iron_worker queue java --wait
+iron_worker queue mono --wait
+iron_worker queue node --wait
+iron_worker queue php --wait
+iron_worker queue python2 --wait
+iron_worker queue python3 --wait
+iron_worker queue ruby1.9 --wait
+iron_worker queue ruby2.0 --wait
+iron_worker queue scala --wait
