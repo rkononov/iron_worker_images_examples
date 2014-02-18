8a3ec5c3f62c6746d61795a79e5a9c2b7be6c804
rkononov <rkononov@gmail.com>
2014-02-18 21:53:41 +0600

    updated images examples (versioned now)

--- launch.sh
+++ launch.sh
@@ -1,19 +1,22 @@
-iron_worker upload java/java
-iron_worker upload mono/mono
-iron_worker upload node/node
-iron_worker upload php/php
-iron_worker upload python2/python2
-iron_worker upload python3/python3
-iron_worker upload ruby1.9/ruby1.9
-iron_worker upload ruby2.0/ruby2.0
-iron_worker upload scala/scala
+#!/bin/sh
+iron_worker upload java-1.7/java
+iron_worker upload mono-2.10/mono
+iron_worker upload mono-3.0/mono3
+iron_worker upload node-0.10/node
+iron_worker upload php-5.4/php
+iron_worker upload python-2.7/python2
+iron_worker upload python-3.2/python3
+iron_worker upload ruby-1.9/ruby1.9
+iron_worker upload ruby-2.1/ruby2.1
+iron_worker upload scala-2.9/scala
 
 iron_worker queue java --wait
 iron_worker queue mono --wait
+iron_worker queue mono3 --wait
 iron_worker queue node --wait
 iron_worker queue php --wait
 iron_worker queue python2 --wait
 iron_worker queue python3 --wait
 iron_worker queue ruby1.9 --wait
-iron_worker queue ruby2.0 --wait
+iron_worker queue ruby2.1 --wait
 iron_worker queue scala --wait
